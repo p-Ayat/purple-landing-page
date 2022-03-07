da5c3aa663e3b96e04d30e7e5e3d6cb63efcb420
Ayat <68519230+p-Ayat@users.noreply.github.com>
2022-03-07 21:33:38 +0200

    js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const x = document.querySelector(".links");
+
+function myFunction() {
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
